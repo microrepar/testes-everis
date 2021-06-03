2435ef682e4f2e4a1f443b059c01930545c5d954
mcsilva <microrepar@gmail.com>
2021-06-03 20:50:54 -0300

    refactor: Adiciona funcao geradora fixture responsavel por iniciar e fechar uma instancia do webdriver.

--- tests/steps_defs/test_carrinho_steps.py
+++ tests/steps_defs/test_carrinho_steps.py
@@ -1,7 +1,7 @@
 """Modulo com a implementacao dos passos para validar 
 o valor total da compra no carrinho
 """
-
+import pytest
 from pytest_bdd import given, parsers, scenario, scenarios, then, when
 from tests.steps_defs.base import Page
 
@@ -11,9 +11,16 @@ EXTRA_TYPES = {"Number": int, "Decimal": float}
 scenarios("../features/carrinho.feature")
 
 
-@given(parsers.cfparse('que estou na loja virtual "{loja}"'), target_fixture="page")
-def page(loja):
+@pytest.fixture
+def page():    
     page: Page = Page()
+    page.driver.implicitly_wait(10)    
+    yield page
+    page.driver.quit()
+
+
+@given(parsers.cfparse('que estou na loja virtual "{loja}"'), target_fixture="home")
+def home(page, loja):    
     page.driver.get(f"http://{loja}/index.php")
     assert page.driver.title == "My Store"
     return page
@@ -35,7 +42,5 @@ def adiciona_produto_carrinho(page: Page, produto, qtde):
     )
 )
 def verifica_total_compra(page: Page, valor_total):
-
     page.efetuar_checkout()
-
     assert valor_total == page.total_carrinho
